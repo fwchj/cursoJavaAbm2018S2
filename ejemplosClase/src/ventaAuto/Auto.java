5ea783674619d17b1b399d755313be7042f4f08d
Florian Chávez-Juárez <florian@chavezjuarez.com>
2018-10-16 17:57:10 -0500

    Update clase 16/10

--- ejemplosClase/src/ventaAuto/Auto.java
+++ ejemplosClase/src/ventaAuto/Auto.java
@@ -1,53 +1,18 @@
+
 package ventaAuto;
 
 
-public class Auto {
-// Variables de instancia
-	String color;
-	String marca;
-	String modelo;
-	byte puertas;
-	double kilometraje;
-	private double precio;
-	boolean electrico;
+public class Auto extends Vehiculo {
 	
-	
-	// Constructor
-	public Auto(String col, String marca, String modelo, int puertas,double km, double p,boolean elec) {
-		this.color = col;
-		this.marca = marca;
-		this.electrico = elec;
-		this.kilometraje = km;
-		this.modelo = modelo;
-		this.puertas = (byte) puertas;
-		this.setPrecio(p);
-		}
-	
-	// Metodo simple en el cual el auto 'se presenta' => imprime un texto en la consola
-	public void presentate() {
-		System.out.printf("Marca %s, Modelo: %s, Color: %s, Precio%s\n", this.marca,this.modelo,this.color,this.getPrecio());
-	}
+	int numeroPasajeros;
 
-
-	// Metodo public para que otras clases puden tener el valor de precio, que definimos como private
-	public double getPrecio() {
-		return precio;
-	}
-
-	// Metodo para que otras clases puedan modificar el precio
-	public void setPrecio(double precio) {
-		this.precio = precio;
+	public Auto(String col, String marca, String modelo, int puertas, double km, double p, boolean elec,int pax) {
+		super(col, marca, modelo, puertas, km, p, elec);
+		this.numeroPasajeros = pax;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public double getPuertasPorPax() {
+		return (double)this.numeroPasajeros / this.puertas;
+	}
+
 }
--- ejemplosClase/src/ventaAuto/Tester.java
+++ ejemplosClase/src/ventaAuto/Tester.java
@@ -7,8 +7,12 @@ public class Tester {
 	public static void main(String[] args) {
 		
 		// Generamos un nuevo Auto y lo guardamos en la variable 'bocho'
-		Auto bocho = new Auto("Amarillo","VW","Bocho", 3, 999383.3,15000.0, false);
+		Vehiculo.contadorCoches();
+
+		Vehiculo bocho = new Auto("Amarillo","VW","Bocho", 3, 999383.3,15000.0, false,4);
+		//bocho.puertas = 4; // no se puede porque es final
 		
+		Vehiculo.contadorCoches();	
 		
 		bocho.presentate(); // Dejamos que se presente el bocho
 		bocho.color = "rojo";	// Cambiamos el color al bocho
@@ -18,13 +22,20 @@ public class Tester {
 		bocho.setPrecio(20000); // Cambiamos el precio: aqui con el metodo, porque no tenemos acceso directo a 'precio'
 
 		System.out.println("=================================");
-		ArrayList<Auto> misCoches = new ArrayList<Auto>();	// Hacemos un array list para todos los coches
+		ArrayList<Vehiculo> misCoches = new ArrayList<Vehiculo>();	// Hacemos un array list para todos los coches
 		
 		misCoches.add(bocho);	// Agregamos el bocho
-		misCoches.add(new Auto("Amarillo","Jeep","Wrangler",5,4550.3,55000,true)); // Generamos un nuevo coche y lo agregamos directamente al AL
+		misCoches.add(new Vehiculo("Amarillo","Jeep","Wrangler",5,4550.3,55000,true)); // Generamos un nuevo coche y lo agregamos directamente al AL
+		System.out.printf("Ncoches:%s\n",Vehiculo.ncoches);
+
+		
+		
+		
+		
+		
 		
 		// Hacemos un loop para que cada coche se 'presente'
-		for(Auto a:misCoches) {
+		for(Vehiculo a:misCoches) {
 			a.presentate();
 		}
 		
